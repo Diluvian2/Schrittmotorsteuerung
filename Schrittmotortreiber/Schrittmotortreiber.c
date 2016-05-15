b1ad5bb3e7f43e9363def9b6a7c77d42ff7418ee
Diluvian <Diluvian.Mail@gmail.com>
2016-05-15 18:52:32 +0200

    Like before, this is using the internal PWM-Generator. Deleted unnecessary lines of code and added comments.
    Check the timer prescaler to get check for which frequencies it's good to use.
    
    Disadvantage of this kind of control is, that it's not easy to implement a function which stops and controls the pwm single-step-precisely.

--- Schrittmotortreiber/Schrittmotortreiber.c
+++ Schrittmotortreiber/Schrittmotortreiber.c
@@ -8,125 +8,42 @@
 
 void ADC_Init(void)
 {
-  // die Versorgungsspannung AVcc als Referenz wählen:
-  //ADMUX = (0<<REFS1) | (0<<REFS0);    
-  // oder interne Referenzspannung als Referenz für den ADC wählen:
-  // ADMUX = (1<<REFS1) | (1<<REFS0);
- 
-  // Bit ADFR ("free running") in ADCSRA steht beim Einschalten
-  // schon auf 0, also single conversion
-  ADCSRA = (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0) | (1<<ADATE);     // Frequenzvorteiler und Free Running Mode Adate
+  ADCSRA = (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0) | (1<<ADATE);     // Prescaler and Free Running Mode
   ADCSRB = (0<<ADTS2) | (0<<ADTS1) | (0<<ADTS0);
-  ADMUX = (ADMUX & ~(0x1F)) | (3 & 0x1F);
-  ADCSRA |= (1<<ADEN);                  // ADC aktivieren
- 
-  /* nach Aktivieren des ADC wird ein "Dummy-Readout" empfohlen, man liest
-     also einen Wert und verwirft diesen, um den ADC "warmlaufen zu lassen" */
- 
-  ADCSRA |= (1<<ADSC);                  // eine ADC-Wandlung 
+  ADMUX = (ADMUX & ~(0x1F)) | (3 & 0x1F); //AD Channel select
+  ADCSRA |= (1<<ADEN);                    //activate ADC 
+  ADCSRA |= (1<<ADSC);                    //one inintial ADC-conversion 
 }
 
-/* ADC Einzelmessung */
-uint16_t ADC_Read( uint8_t channel)
-{
-	static uint16_t last_val;
-	
-	if( ADCSRA & (1<<ADSC) )
-	{	//Konvertierung noch nicht abgeschlossen
-		return last_val;
-	}
-	else
-	{
-		// Kanal waehlen, ohne andere Bits zu beeinflußen
-		ADMUX = (ADMUX & ~(0x1F)) | (3 & 0x1F);
-		ADCSRA |= (1<<ADSC);					// eine Wandlung "single conversion"
-		last_val = ADCW;
-		return ADCW;                    // 16bit ADC auslesen und zurückgeben
-	}
-}
-
-void delay_us(int ms)
-{
-	while (ms--) {
-		_delay_us(1);  // one millisecond
-	}
-}
-
-void turn_motor(int16_t *steptime, int16_t *adcval, int16_t *zaehler) {
-	static uint16_t deltav_zaehler = 0;
-	static uint16_t deltav_zmax = 1500;
-	static uint16_t powerduration_steps = 0;
-	powerduration_steps =  *steptime>>1;
-	
-	if(*zaehler >= powerduration_steps)
-	{
-		PORTA = 0x01;
-	}
-	
-		
-	if(*zaehler >= *steptime)
-	{
-		*zaehler = 0;
-		PORTA = 0b00000011;			
-	}
-	
-  	if(deltav_zaehler > deltav_zmax)
-  	{
-		if(*adcval > *steptime)
-			{
-				*steptime += 1;
-				deltav_zaehler = 0;
-			}
-			else if(*adcval < *steptime)
-			{
-				*steptime -= 1;
-				deltav_zaehler = 0;
-			}
-}
-	deltav_zaehler++;
-}
-
-void setAdValuetoTimerTOP() {
-	unsigned char sreg;
-	/* Save global interrupt flag */
-	sreg = SREG;
-	/* Disable interrupts */
-	cli();
-	/* Set TCNT0 to AD Value */
-	
+void setTimerFromADC() {	
+	//Write 10-Bit Timer register with 16-Bit AD-Value ADCW
 	TC1H = ADCW>>8;
 	OCR1C = ADCW;
-	/* Restore global interrupt flag */
-	SREG = sreg;
 }
 
 
 int main(void)
 {
-// 	Change CLOCK
-// 		CLKPR  = (1<<CLKPCE);
-// 		CLKPR = (0<<CLKPS1) | (0<<CLKPS1) | (0<<CLKPS2) | (0<<CLKPS3);
 
-	DDRA = 0x0F; //Stepper Motor ouputs PA0 - PA3	
-	DDRB = (1<<DDB1);
-	// Timer 0 ist für die Motor-Schritte zuständig    
+	DDRB = (1<<DDB1); //Set Port B1 to OUTPUT
+	PORTA = (1<<PORTA0)	; //Activate Stepper-Driver-IC
+	
+	// PWM Timer Setup
 	TCCR1A = (1<<COM1A0);
-	TCCR1B = (1<<CS12) ;
-	//Timer-TOP-Registers
+	TCCR1B = (1<<CS11) | (1<<CS10); //Prescaler = CK/8
+	//Timer-TOP-Registers (initial values, irrelevant for later use)
 	TC1H = 0x06;
 	OCR1C  = 0x1;
-	
-
-	TIMSK |=  (1<<OCIE1A)  ;  //Interrupt enable	
 
 	ADC_Init();  
-	// Interrupts global freigeben  
+	
+	// clear Timer (not necessary in this case)
 	TC1H = 0;
 	TCNT1 = 0;	
 
-	PORTA = (1<<PORTA0)	;
+	
 	while(1)
 	{	
-		setAdValuetoTimerTOP();		
+		setTimerFromADC();		
 	}
 }
